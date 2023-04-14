f8db45a87eeda42016aca8b86d1deb9673e4f5df
Mattes D <github@xoft.cz>
2023-04-14 16:48:57 +0200

    SaveSnapshotsWhileAlarm: Added saving snapshots to separate files.

--- Apps/SaveSnapshotsWhileAlarm/Main.cpp
+++ Apps/SaveSnapshotsWhileAlarm/Main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
 #include "fmt/format.h"
 #include "Recorder.hpp"
 #include "Root.hpp"
@@ -7,6 +9,7 @@
 
 
 
+/** Stores the info about a single device that should be monitored. */
 struct MonitoredDevice
 {
 	std::string mHostName;
@@ -14,19 +17,24 @@ struct MonitoredDevice
 	std::string mUserName;
 	std::string mPassword;
 	std::shared_ptr<NetSurveillancePp::Recorder> mRecorder;
-	std::atomic<bool> mShouldScheduleSnapshots;
 
 	MonitoredDevice(std::string && aHostName, uint16_t aPort, std::string && aUserName, std::string && aPassword):
 		mHostName(std::move(aHostName)),
 		mPort(std::move(aPort)),
 		mUserName(std::move(aUserName)),
-		mPassword(std::move(aPassword)),
-		mShouldScheduleSnapshots(false)
+		mPassword(std::move(aPassword))
 	{
 	}
 };
 
-using MonitoredDevicePtr = std::unique_ptr<MonitoredDevice>;
+using MonitoredDevicePtr = std::shared_ptr<MonitoredDevice>;
+
+/** Stores one channel of a particular device for which an alarm has started. */
+struct ActiveAlarm
+{
+	MonitoredDevicePtr mDevice;
+	int mChannel;
+};
 
 
 
@@ -35,9 +43,17 @@ using MonitoredDevicePtr = std::unique_ptr<MonitoredDevice>;
 /** The devices that should be monitored for alarms. */
 std::vector<MonitoredDevicePtr> gMonitoredDevices;
 
+/** All the alarms that are currently active (snapshots should be saved every second).
+Protected against multithreaded access by gMtxActiveAlarms. */
+std::vector<ActiveAlarm> gActiveAlarms;
+
+/** The mutex protecting gActiveAlarms agains multithreaded access. */
+std::mutex gMtxActiveAlarms;
+
 /** The name of the DB file where to store the snapshots.
 If empty, the snapshots are stored in the current folder. */
 std::string gDbFileName;
+
 
 
 
@@ -84,7 +100,7 @@ bool addMonitoredDevice(const std::string & aDeviceSpec)
 		hostName = aDeviceSpec.substr(idxAt + 1);
 	}
 
-	gMonitoredDevices.push_back(std::make_unique<MonitoredDevice>(
+	gMonitoredDevices.push_back(std::make_shared<MonitoredDevice>(
 		std::move(hostName), port, std::move(userName), std::move(password)
 	));
 	return true;
@@ -136,9 +152,62 @@ bool parseCommandLine(int aArgC, char ** aArgV)
 
 
 
+/** Saves the actual received snapshot data for the device's channel. */
+void saveSnapshotData(std::shared_ptr<MonitoredDevice> aDevice, int aChannel, const char * aData, size_t aSize)
+{
+	auto tt = std::time(nullptr);
+	auto tm = std::localtime(&tt);
+	auto yearStr = fmt::format("{:04d}", tm->tm_year + 1900);
+	auto yearMonthStr = fmt::format("{}/{:02d}", yearStr, tm->tm_mon);
+	auto dateStr = fmt::format("{}/{:02d}", yearMonthStr, tm->tm_mday);
+	std::filesystem::create_directory(yearStr);
+	std::filesystem::create_directory(yearMonthStr);
+	std::filesystem::create_directory(dateStr);
+	auto fnam = fmt::format(
+		"{}/{:02d}_{:02d}_{:02d}_{}_{}_ch{}.jpg",
+		dateStr, tm->tm_hour, tm->tm_min, tm->tm_sec, aDevice->mHostName, aDevice->mPort, aChannel
+	);
+	std::ofstream f;
+	f.open(fnam.c_str(), std::ofstream::binary | std::ofstream::out);
+	if (!f.is_open())
+	{
+		std::cerr << fmt::format(
+			"Failed to write snapshot from device {}:{}, channel {}, to file {}.\n",
+			aDevice->mHostName, aDevice->mPort, aChannel, fnam
+		);
+		return;
+	}
+	f.write(aData, aSize);
+	f.close();
+}
+
+
+
+
+
+/** Saves a snapshot from the specified device's channel. */
+void saveSnapshot(std::shared_ptr<MonitoredDevice> aDevice, int aChannel)
+{
+	aDevice->mRecorder->capturePicture(aChannel,
+		[aDevice, aChannel](const std::error_code & aError, const char * aData, size_t aSize)
+		{
+			if (aError)
+			{
+				std::cerr << fmt::format("Failed to capture snapshot from device {}:{}: {}\n", aDevice->mHostName, aDevice->mPort, aError.message());
+				return;
+			}
+
+			saveSnapshotData(aDevice, aChannel, aData, aSize);
+		}
+	);
+}
+
+
+
+
+
 /** Called whenever an alarm event comes from a device.
-Saves a snapshot from the affected channel.
-TODO: A snapshot should be saved every second until the alarm goes out. */
+Schedules a snapshot of the affected channel to be saved every second until the alarm goes out. */
 void onAlarm(
 	MonitoredDevicePtr & aDevice,
 	const std::error_code & aError,
@@ -153,13 +222,28 @@ void onAlarm(
 		std::cerr << fmt::format("Failed to monitor alarms on {}:{}: {}", aDevice->mHostName, aDevice->mPort, aError.message());
 		return;
 	}
-	if (!aIsStart)
+	if (aIsStart)
 	{
-		return;
-	}
+		// Save the first snapshot of the series ASAP:
+		saveSnapshot(aDevice, aChannel);
 
-	// TODO: Save the snapshot
-	// TODO: Schedule saving a snapshot every second until the alarm goes out
+		// Add the device's channel to the active alarms:
+		std::cout << fmt::format("AlarmStart: Device {}:{}, channel {}\n", aDevice->mHostName, aDevice->mPort, aChannel);
+		std::unique_lock<std::mutex> lg(gMtxActiveAlarms);
+		gActiveAlarms.push_back({aDevice, aChannel});
+	}
+	else
+	{
+		// Remove the device's channel from the active channels:
+		std::cout << fmt::format("AlarmEnd: Device {}:{}, channel {}\n", aDevice->mHostName, aDevice->mPort, aChannel);
+		std::unique_lock<std::mutex> lg(gMtxActiveAlarms);
+		gActiveAlarms.erase(std::remove_if(gActiveAlarms.begin(), gActiveAlarms.end(),
+			[&aDevice, aChannel](const auto & aAlarm)
+			{
+				return (aAlarm.mDevice == aDevice) && (aAlarm.mChannel == aChannel);
+			}
+		));
+	}
 }
 
 
@@ -220,6 +304,36 @@ bool startMonitoring()
 
 
 
+/** Called periodically by ASIO.
+Saves a snapshot for each active alarm's channel. */
+void onTimer (asio::steady_timer & aTimer, const std::error_code & aError)
+{
+	if (aError)
+	{
+		std::cerr << fmt::format("Failed to set up timer: {}\n", aError.message());
+		return;
+	}
+
+	// Get a copy of all the active alarms
+	std::vector<ActiveAlarm> activeAlarms;
+	{
+		std::unique_lock<std::mutex> lock(gMtxActiveAlarms);
+		activeAlarms = gActiveAlarms;
+	}
+	for (const auto & aa: activeAlarms)
+	{
+		saveSnapshot(aa.mDevice, aa.mChannel);
+	}
+
+	// Set up the next timer tick:
+	aTimer.expires_after(asio::chrono::seconds(1));
+	aTimer.async_wait(std::bind(&onTimer, std::ref(aTimer), std::placeholders::_1));
+};
+
+
+
+
+
 int main(int aArgC, char ** aArgV)
 {
 	if (!parseCommandLine(aArgC, aArgV))
@@ -232,6 +346,12 @@ int main(int aArgC, char ** aArgV)
 		std::cerr << "Failed to start monitoring.\n";
 		return 2;
 	}
+
+	// Set up the timer for periodic snapshotting:
+	asio::steady_timer timer(NetSurveillancePp::Root::instance().ioContext(), asio::chrono::seconds(1));
+	timer.async_wait(std::bind(&onTimer, std::ref(timer), std::placeholders::_1));
+
+	// Run it all asynchronously:
 	NetSurveillancePp::Root::instance().ioContext().run();
 	return 0;
 }
